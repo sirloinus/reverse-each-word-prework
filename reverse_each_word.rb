fedfae0534d77a366a7c36f336a51fa68c547010
Amalie S <amaliesmidth@gmail.com>
2018-08-28 11:24:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence1)
+  sentence1_array = sentence1.split(" ")
+  sentence1_array_2 = []
+  sentence1_array.each do |word|
+    word.reverse!
+    sentence1_array_2 << word
+  end
+  sentence1_array_2.join(" ")
+end
+
